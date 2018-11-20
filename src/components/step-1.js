3abe8f0b566f10faf6d2e3720cde3d3473c8daa0
nakulnagariy <nagariya23nakul@gmail.com>
2018-11-20 08:28:46 +0530

    step-1 dom is ready

--- src/components/step-1.js
+++ src/components/step-1.js
@@ -4,28 +4,42 @@ class Step1 extends Component {
     constructor(props){
         super(props);
         this.state = {
-            title: "mr",
+            ownerTitle: "mr",
+            ownerName: "Jhon Miller",
+            agreementDate: new Date(),
+            monthlyRent: 2000,
         }
-
-        this.handleSelect = this.handleSelect.bind(this);
+        this.handleMe = this.handleMe.bind(this);
     }
 
-    handleSelect(e) {
-        console.log(e.target.value)
+    handleMe(e) {
+        this.setState({
+            [e.target.name]: e.target.value,
+        })
     }
-
-
 
     render () {
         return (
             <React.Fragment>
                 <div className="input-group">
-                    <label htmlFor="owner-title">Title:</label>
-                    <select name="owner-title" id="owner-title" onChange={this.handleSelect}>
+                    <label htmlFor="ownerTitle">Title:</label>
+                    <select name="ownerTitle" id="ownerTitle" onChange={this.handleMe}>
                         <option value="mr" >Mr</option>
                         <option value="ms">Ms</option>
                         <option value="mrs">Mrs</option>
                     </select>
+                </div>
+                <div className="input-group">
+                    <label htmlFor="ownerName">Name:</label>
+                    <input type="text" id="ownerName" name = "ownerName" onChange={this.handleMe} placeholder={this.state.ownerName}/>
+                </div>
+                <div className="input-group">
+                    <label htmlFor="agreementDate">Agreement date:</label>
+                    <input type="date" id="agreementDate" name="agreementDate" onChange={this.handleMe} placeholder={this.state.agreementDate}/>
+                </div>
+                <div className="input-group">
+                    <label htmlFor="monthlyRent">Monthly rent:</label>
+                    <input type="number" id="monthlyRent" name="monthlyRent" onChange={this.handleMe} placeholder={this.state.monthlyRent}/>
                 </div>
             </React.Fragment>
         )
